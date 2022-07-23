0853a176e117124c36339813273320babf3e07ce
me-neeraj135 <me.neeraj1455@gmail.com>
2022-07-23 15:53:06 +0530

    absolute-relative-path-bloack-2 assignment completed

---
+++ block-BNaaax/sample/server.js
@@ -0,0 +1,7 @@
+let path = require(`path`);
+let formPath = `./form.html`;
+// console.log(__dirname);
+// console.log(__filename);
+
+let filesPath = path.join(__dirname, formPath);
+console.log(filesPath);
