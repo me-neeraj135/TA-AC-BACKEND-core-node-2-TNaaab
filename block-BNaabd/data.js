6f25a3766625b23819042aaf4b0e8d7041e56db0
me-neeraj135 <me.neeraj1455@gmail.com>
2022-07-24 21:46:18 +0530

    capturing data block4 completed

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -7,34 +7,22 @@ let server = http.createServer(handleRequest);
 function handleRequest(req, res) {
   let dataFormate = req.headers[`content-type`];
   let store = ``;
-  console.log(dataFormate, req.url);
 
-  if (req.method === `POST` && req.url === `/form`) {
-    req.on(`data`, chunk => {
-      store += chunk;
-    });
+  req.on(`data`, chunk => {
+    store += chunk;
+  });
 
-    req.on(`end`, () => {
-      if (dataFormate === `application/x-www-form-urlencoded`) {
-        let parseData = qs.parse(store);
-        res.end(JSON.stringify(parseData));
-      }
-    });
-  } else if (req.method === `POST` && req.url === `/JSON`) {
-    req.on(`data`, chunk => {
-      store += chunk;
-    });
-
-    req.on(`end`, () => {
-      if (dataFormate === `application/json`) {
-        let parseData = JSON.parse(store);
-        res.end(store);
-      }
-    });
-  } else {
-    res.writeHead(404, { ContentType: `text/html` });
-    res.end(`<h2>page not found</h2>`);
-  }
+  req.on(`end`, () => {
+    if (req.method === `POST` && req.url === `/json`) {
+      let parseData = JSON.parse(store);
+      res.setHeader(`content-type`, `application/json`);
+      res.end(store);
+    }
+    if (req.method === `POST` && req.url === `/form`) {
+      let parseData = qs.parse(store);
+      res.end(JSON.stringify(parseData));
+    }
+  });
 }
 
 server.listen(7000, () => {
